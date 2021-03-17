afbf3c064c18700385c20021559b5848cbfc3a56
DavidRMorphew <morphewd@umich.edu>
2021-03-17 18:47:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const newArray = []
+    for (let index = 0; index < namesArray.length; index++) {
+        newArray.push(`Thank you, ${namesArray[index]}, for the wonderful ${eventName} gift!`)
+    };
+    return newArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    };
+}
